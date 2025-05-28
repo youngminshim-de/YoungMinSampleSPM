cb38d530f6dbdf28c04237ba0703e135de745e27
shimyoungmin <ym.sim@cardoc.kr>
2025-05-28 14:40:02 +0900

    create project

---
+++ YoungMinSampleSPM/Sources/YoungMinSampleSPM/YoungMinSampleSPM.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+public class Sample {
+    public init() {}
+    
+    public func test() {
+        print("Test!!")
+    }
+}
